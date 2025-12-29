31cea13a869a60b7fb140ebf7b8a57a3dbdb0393
admin <rahulreddy1809@gmail.com>
2025-12-29 23:10:51 +0530

    29 Dec 2025 - All files

---
+++ src/ex_03_Identifier(Literals)_Variables/Lab029_Task1.py
@@ -0,0 +1,18 @@
+# Task for the day
+# Take 3 input from the users
+# Perform add, sub, mul, div
+
+num1 = float(input("Enter the numb1 : "))
+num2 = float(input("Enter the numb2 : "))
+num3 = float(input("Enter the numb3 : "))
+
+add_result = num1 + num2 + num3
+sub_result = num1 - num2 - num3
+mul_result = num1 * num2 * num3
+div_result = (num1/num2) / num3
+
+print(add_result)
+print(sub_result)
+print(mul_result)
+print(div_result)
+
---
+++ src/ex_03_Identifier(Literals)_Variables/Lab030_Task2.py
@@ -0,0 +1,10 @@
+from math import remainder
+
+num1 = float(input("Enter num1 : "))
+num2 = float(input("Enter num2 : "))
+
+quotient = num1 // num2
+remainder = num1 % num2
+
+print(quotient)
+print(remainder)
