3e3d8390f77ca170d2f401d68e9356d90219361a
admin <rahulreddy1809@gmail.com>
2026-01-03 17:02:15 +0530

    30 Dec 2025 - All files

---
+++ Task/Task_04.py
@@ -0,0 +1,8 @@
+# write a programme to find area of a triangle
+# Area of triangle = 1/2 * base * height
+# base = 10, height = 5
+# print the result using Arithemetic Operators
+
+Area_of_triangle = 1/2 * 10 * 5
+print("Area of triangle is : ", Area_of_triangle)
+
---
+++ Task/Task_05.py
@@ -0,0 +1,10 @@
+# write a programme to check if a number entered by the user is greater than 100
+
+
+number = float(input("please enter any number greater than 100 : "))
+
+if number >= 100:
+    print(f"the entered number {number} is greater than 100 : ")
+else:
+    print(f"the entered number {number} is less than 100 : ")
+
